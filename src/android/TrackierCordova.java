b8162661e3ffeff8e08f2f8cd5b47b0a50e14041
Hemant Mann <hemant.mann121@gmail.com>
2021-04-07 15:29:09 +0530

    fix: java class error

--- src/android/TrackierCordova.java
+++ src/android/TrackierCordova.java
@@ -13,47 +13,47 @@ import java.util.*;
  */
 public class TrackierCordova extends CordovaPlugin {
 
-    @Override
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("initialize")) {
-            initialize(args);
-            return true;
-        } else if (action.equals("trackEvent")) {
-            trackEvent(args);
-            return true;
-        }
-        return false;
-    }
+	@Override
+	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+		if (action.equals("initialize")) {
+			initialize(args);
+			return true;
+		} else if (action.equals("trackEvent")) {
+			trackEvent(args);
+			return true;
+		}
+		return false;
+	}
 
-    public void initialize(JSONArray args) {
-        String params = args.getString(0);
-        JSONArray jsonArrayParams = new JSONArray(params);
-        JSONObject jsonParameters = jsonArrayParams.optJSONObject(0);
-        Map<String, Object> parameters = TrackierCordovaUtil.jsonObjectToMap(jsonParameters);
+	public void initialize(JSONArray args) throws JSONException {
+		String params = args.getString(0);
+		JSONArray jsonArrayParams = new JSONArray(params);
+		JSONObject jsonParameters = jsonArrayParams.optJSONObject(0);
+		Map<String, Object> parameters = TrackierCordovaUtil.jsonObjectToMap(jsonParameters);
 
-        String appToken = null;
-        String environment = null;
-        if (parameters.containsKey(TrackierCordovaUtil.KEY_APP_TOKEN)) {
-            appToken = parameters.get(TrackierCordovaUtil.KEY_APP_TOKEN).toString();
-        }
-        if (parameters.containsKey(TrackierCordovaUtil.KEY_ENVIRONMENT)) {
-            appToken = parameters.get(TrackierCordovaUtil.KEY_ENVIRONMENT).toString();
-        }
+		String appToken = null;
+		String environment = null;
+		if (parameters.containsKey(TrackierCordovaUtil.KEY_APP_TOKEN)) {
+			appToken = parameters.get(TrackierCordovaUtil.KEY_APP_TOKEN).toString();
+		}
+		if (parameters.containsKey(TrackierCordovaUtil.KEY_ENVIRONMENT)) {
+			appToken = parameters.get(TrackierCordovaUtil.KEY_ENVIRONMENT).toString();
+		}
 		TrackierSDKConfig sdkConfig = new TrackierSDKConfig(this.cordova.getActivity().getApplicationContext(), appToken, environment);
 		TrackierSDK.initialize(sdkConfig);
 	}
 
-    public void trackEvent(JSONArray args) {
-        String params = args.getString(0);
-        JSONArray jsonArrayParams = new JSONArray(params);
-        JSONObject jsonParameters = jsonArrayParams.optJSONObject(0);
-        Map<String, Object> parameters = TrackierCordovaUtil.jsonObjectToMap(jsonParameters);
+	public void trackEvent(JSONArray args) throws JSONException {
+		String params = args.getString(0);
+		JSONArray jsonArrayParams = new JSONArray(params);
+		JSONObject jsonParameters = jsonArrayParams.optJSONObject(0);
+		Map<String, Object> parameters = TrackierCordovaUtil.jsonObjectToMap(jsonParameters);
 
-        String eventId = "";
-        if (parameters.containsKey(TrackierCordovaUtil.KEY_EVENT_ID)) {
-            eventId = parameters.get(TrackierCordovaUtil.KEY_EVENT_ID).toString();
-        }
-        TrackierEvent evtObj = new TrackierEvent(eventId);
-        TrackierSDK.trackEvent(evtObj);
-    }
+		String eventId = "";
+		if (parameters.containsKey(TrackierCordovaUtil.KEY_EVENT_ID)) {
+			eventId = parameters.get(TrackierCordovaUtil.KEY_EVENT_ID).toString();
+		}
+		TrackierEvent evtObj = new TrackierEvent(eventId);
+		TrackierSDK.trackEvent(evtObj);
+	}
 }
